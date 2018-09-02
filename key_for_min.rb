f24b5bf3b974b25b07a2f9793b1679a5790d4a64
Jacqueline Chenault <adigitalnative@gmail.com>
2018-09-02 21:02:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_value_name = nil
   
   name_hash.each do |name, value|
-    raise value.inspect
+    raise name.inspect
     if min_value && min_value > value
       min_value = value
       min_value_name = name
